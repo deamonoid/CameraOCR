8cff3693f323d2e6dcf9f692441a388e7ec1b1b3
Ghost <deamonoid@users.noreply.github.com>
2017-07-19 21:46:50 +0530

    Implement OCR

--- app/src/main/java/com/example/ghost/cameraocr/MainActivity.java
+++ app/src/main/java/com/example/ghost/cameraocr/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ghost.cameraocr;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.hardware.Camera;
@@ -11,14 +12,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-
+    Bitmap resizedBitmap;
+    String datapath = "";
+    private TessBaseAPI mTess;
     private Camera mCamera;
     private CameraPreview mPreview;
 
@@ -47,6 +59,17 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+
+
+        //initializing Tesseract API
+        String language = "eng";
+        datapath = getFilesDir() + "/tesseract/";
+        mTess = new TessBaseAPI();
+
+        checkFile(new File(datapath + "tessdata/"));
+
+        mTess.init(datapath, language);
+
 
         //AutoFocus Mode
         Camera.Parameters parameters = mCamera.getParameters();
@@ -96,7 +119,61 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private Bitmap scaleDownBitmapImage(Bitmap bitmap, int newWidth, int newHeight) {
-        Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, true);
+        resizedBitmap = Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, true);
         return resizedBitmap;
+    }
+
+    //Tesseract Code
+    private void checkFile(File dir) {
+        if (!dir.exists() && dir.mkdirs()) {
+            copyFiles();
+        }
+        if (dir.exists()) {
+            String datafilepath = datapath + "/tessdata/eng.traineddata";
+            File datafile = new File(datafilepath);
+
+            if (!datafile.exists()) {
+                copyFiles();
+            }
+        }
+    }
+
+    private void copyFiles() {
+        try {
+            String filepath = datapath + "/tessdata/eng.traineddata";
+            AssetManager assetManager = getAssets();
+
+            InputStream instream = assetManager.open("tessdata/eng.traineddata");
+            OutputStream outstream = new FileOutputStream(filepath);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = instream.read(buffer)) != -1) {
+                outstream.write(buffer, 0, read);
+            }
+
+
+            outstream.flush();
+            outstream.close();
+            instream.close();
+
+            File file = new File(filepath);
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void processImage(View view) {
+
+        String OCRresult = null;
+        mTess.setImage(resizedBitmap);
+        OCRresult = mTess.getUTF8Text();
+        TextView OCRTextView = (TextView) findViewById(R.id.OCRTextView);
+        OCRTextView.setText(OCRresult);
     }
 }
